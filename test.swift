f488067d26a9115c8e9d51b7fd6a5cc5e1c74700
Ameymae Swift <71127707+AmeyMae@users.noreply.github.com>
2020-09-30 02:43:21 -0500

    Add files via upload

---
+++ test.swift
@@ -0,0 +1,33 @@
+var str = "Welcome"
+var num1 = 2
+var num2 = 5
+var str2:String = "hi"
+var num3 = 3.4
+
+print(str)
+print(num1 + num2)
+print(str2)
+print(num3)
+
+var num4:Double = 3.45
+var num:Float = 3.92
+
+if(num4 > 3 && num4 > 4)
+{
+	print("hello")
+}
+
+else if(num4 > 3.4 || num4 < 100)
+{
+	print("hello")
+}
+
+else
+{
+	print("whatever")
+}
+
+for index in 1...5
+{
+	print("Stuff")
+}
